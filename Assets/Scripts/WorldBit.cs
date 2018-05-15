a6c291a8aaac4b5b6ff80ec88fa13bbb7ece3c09
Ermal Ferati <ermalferati@gmail.com>
2018-05-15 17:07:06 +0200

    Added source code

---
+++ Assets/Scripts/WorldBit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WorldBit : MonoBehaviour
+{
+    public Vector3 size;
+    public WorldBit[] matchingWorldBits;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1);
+        WorldGeneration.instance.generateWorldBit(size);
+    }
+}
---
+++ Assets/Scripts/WorldGeneration.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class WorldGeneration : MonoBehaviour
+{
+    public static WorldGeneration instance;
+    public WorldBit currentWorldBit;
+
+    private Vector3 positionToSpawn;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        generateWorldBit(Vector3.zero);
+    }
+
+    public void generateWorldBit(Vector3 distance)
+    {
+        Instantiate(getMatchingWorldBit(), positionToSpawn += distance, Quaternion.identity);
+    }
+
+    private WorldBit getMatchingWorldBit()
+    {
+        return currentWorldBit = currentWorldBit.matchingWorldBits[Random.Range(0, currentWorldBit.matchingWorldBits.Length)]; 
+    }
+}
